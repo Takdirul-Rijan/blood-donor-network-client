76745c5a262d5026ed63f7c019a094d6b6fa2193
Takdirul-Rijan <takdirul.rijan@gmail.com>
2025-12-08 09:46:37 +0600

    Added Dashboard Layout with sidebar and Outlet for nested routes

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Outlet } from "react-router";
+import DashboardSidebar from "../components/DashboardSidebar/DashboardSidebar";
 
 const DashboardLayout = () => {
   return (
-    <div>
-      <h2>Dashboard Layout</h2>
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-12">
+      <div className="lg:col-span-3 bg-base-200 min-h-screen p-5">
+        <DashboardSidebar></DashboardSidebar>
+      </div>
+
+      <div className="lg:col-span-9 p-5">
+        <Outlet></Outlet>
+      </div>
     </div>
   );
 };
